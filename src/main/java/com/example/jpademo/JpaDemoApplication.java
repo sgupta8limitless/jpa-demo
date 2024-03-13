40b0b7f8f367f707a3426fcc7325d4db0416edbf
sgupta8limitless <sgupta8mz@gmail.com>
2024-03-13 23:16:01 +0530

    Update JpaDemoApplication.java

--- src/main/java/com/example/jpademo/JpaDemoApplication.java
+++ src/main/java/com/example/jpademo/JpaDemoApplication.java
@@ -4,6 +4,7 @@ package com.example.jpademo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//hello
 
 @SpringBootApplication
 public abstract class JpaDemoApplication {
